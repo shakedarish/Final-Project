b49ac0767574be14511149a0c31df414df4f6ce8
seanfr529 <seanfr529@gmail.com>
2024-02-14 21:14:22 +0200

    ! to example

--- server/server1/index.js
+++ server/server1/index.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const cors = require('cors'); // Import the cors middleware
+const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 const port = 3003;
 //const {Configuration, OpenAIApi} = require("opanai");
 
 app.use(cors()); // Use the cors middleware
 app.use(express.json());
-require('dotenv').config();
+require("dotenv").config();
 //const apiKey = process.env.OPEN_AI_KEY;
-const apiKey = "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";
-app.post('/completions', async (req, res) => {
-  try{
+const apiKey = "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";
+app.post("/completions", async (req, res) => {
+  try {
     const message = req.body.message;
     const options = {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${apiKey}`,
-        "Content-Type": "application/json"
-    },
-    body:JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: message }],
-      max_tokens: 100,
-    })
-  };
-    const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-    const data = await response.json()
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: message }],
+        max_tokens: 100,
+      }),
+    };
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
     res.json(data);
-}catch(error){
-  console.error("Error generating completion:", error);
-  res.status(500).json({error: 'Internal server error'});
-}
-    //videos.push(newVideo);
-    // res.status(201).json(newVideo);
+  } catch (error) {
+    console.error("Error generating completion:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+  //videos.push(newVideo);
+  // res.status(201).json(newVideo);
   // } else {
   //   res.status(400).json({ error: 'Invalid request, please provide data in the request body.' });
 });
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import '../App.css';
-import  Button  from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import Button from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='h-screen flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat' style={{ backgroundImage: 'url("/images/img-home.jpg")' }}>
-        <h1 className='text-7xl font-bold text-white'>VidWizard</h1>
-      <p className='text-white text-2xl m-5'>What are you waiting for?</p>
-      <div className='hero-btns'>
+    <div
+      className="h-screen flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: 'url("/images/img-home.jpg")' }}
+    >
+      <h1 className="text-7xl font-bold text-white">VidWizard</h1>
+      <p className="text-white text-2xl m-5">What are you waiting for?</p>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-          linkPath='/createForm'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          linkPath="/createForm"
         >
           GET STARTED
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          linkPath=''
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          linkPath=""
         >
-         EXAMPLES <i className='far fa-play-circle' />
+          EXAMPLES! <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
-
-      
-      
-
   );
 }
 
