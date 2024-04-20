cdb5da408de7d8c05de2dbcbdd5caaa2da5fe2fc
shaked07 <shakedarish70@gmail.com>
2024-04-20 23:57:41 +0300

    video page styles

--- src/components/pages/VideoPage.jsx
+++ src/components/pages/VideoPage.jsx
@@ -56,56 +56,57 @@ const VideoSeciton = () => {
         }}
       ></div>
       <div className="w-full h-full rounded-full absolute top-0 right-10rem -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-blue-50 via-cyan-100 to-cyan-50"></div>
-      <div className="h-full w-full flex flex-col justify-start items-center gap-8 ">
-        <h1 className="mt-10 mb-10 font-bold text-6xl font-[kalam-bold] custom-text-shadow">
-          Your generated video
-        </h1>
-        <div>
-          <video controls width="960" height="540" autoPlay loop>
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div className="mt-10 mb-10 flex justify-center">
-          <img
-            className=""
-            src={downloadIcon}
-            alt="Previous"
-            onClick={handleDownload}
-            style={{
-              cursor: "pointer",
-              marginRight: "10px",
-              transform: "scaleX(-1)",
-              height: "50px",
-            }}
-          ></img>
-          <FacebookShareButton
-            url={"https://www.google.com/"}
-            quote={"testtt"}
-            hashtag={"#vidoe Created By VidWizard"}
-          >
-            <FacebookIcon size={50} round={true} className="ml-10" />
-          </FacebookShareButton>
-          <WhatsappShareButton url={videoUrl}>
-            <WhatsappIcon size={50} className="ml-10" round={true} />
-          </WhatsappShareButton>
-          <TelegramShareButton url={videoUrl}>
-            <TelegramIcon size={50} className="ml-10" round={true} />
-          </TelegramShareButton>
-          <EmailShareButton url={videoUrl}>
-            <EmailIcon size={50} className="ml-10" round={true} />
-          </EmailShareButton>
-          <TwitterShareButton url={videoUrl}>
-            <TwitterIcon size={50} className="ml-10" round={true} />
-          </TwitterShareButton>
-        </div>
-        <div></div>
+      <div className="bg-white bg-opacity-65  rounded-xl w-2/4  items-center p-10 mx-auto my-auto">
+        <div className="h-full w-full flex flex-col justify-start items-center gap-8 ">
+          <h1 className="mt-10 mb-10 font-bold text-6xl font-[kalam-bold] custom-text-shadow">
+            Your generated video
+          </h1>
+          <div>
+            <video controls width="960" height="540" autoPlay loop>
+              <source src={videoUrl} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="mt-10 mb-10 flex justify-center">
+            <img
+              className=""
+              src={downloadIcon}
+              alt="Previous"
+              onClick={handleDownload}
+              style={{
+                cursor: "pointer",
+                marginRight: "10px",
+                transform: "scaleX(-1)",
+                height: "50px",
+              }}
+            ></img>
+            <FacebookShareButton
+              url={"https://www.google.com/"}
+              quote={"testtt"}
+              hashtag={"#vidoe Created By VidWizard"}
+            >
+              <FacebookIcon size={50} round={true} className="ml-10" />
+            </FacebookShareButton>
+            <WhatsappShareButton url={videoUrl}>
+              <WhatsappIcon size={50} className="ml-10" round={true} />
+            </WhatsappShareButton>
+            <TelegramShareButton url={videoUrl}>
+              <TelegramIcon size={50} className="ml-10" round={true} />
+            </TelegramShareButton>
+            <EmailShareButton url={videoUrl}>
+              <EmailIcon size={50} className="ml-10" round={true} />
+            </EmailShareButton>
+            <TwitterShareButton url={videoUrl}>
+              <TwitterIcon size={50} className="ml-10" round={true} />
+            </TwitterShareButton>
+          </div>
 
-        <EditButton
-          text="Done"
-          onClick={() => {}}
-          additionalClass="mb-16 bg-zinc-800 hover:bg-zinc-900 text-white"
-        />
+          <EditButton
+            text="Done"
+            onClick={() => {}}
+            additionalClass="mb-16 bg-zinc-800 hover:bg-zinc-900 text-white"
+          />
+        </div>
       </div>
     </>
   );
