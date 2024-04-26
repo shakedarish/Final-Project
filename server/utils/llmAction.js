f9ca56237053304db407a84298e1801de4e3764c
Shaked Arish <100476023+shakedarish@users.noreply.github.com>
2024-04-26 14:29:27 +0300

    Update llmAction.js for no call

--- server/utils/llmAction.js
+++ server/utils/llmAction.js
@@ -30,29 +30,29 @@ const chatCompletion = async (systemContent, userContent) => {
   };
   console.log(temp);
 
-  try {
-    const response = await fetch(baseUrl, requestBody);
-    if (!response.ok) {
-      console.error(
-        `Error in chatComplition: ${response.status}, error: ${response.statusText}`
-      );
-      return null;
-    }
+  // try {
+  //   const response = await fetch(baseUrl, requestBody);
+  //   if (!response.ok) {
+  //     console.error(
+  //       `Error in chatComplition: ${response.status}, error: ${response.statusText}`
+  //     );
+  //     return null;
+  //   }
 
-    const data = await response.json();
-    const textResponse = data.choices[0]?.message?.content ?? "";
-
-    if (!textResponse) {
-      //handle case of empty resoult
-      console.log("response was empty");
-      return null;
-    }
-    console.log("textResponse: " + textResponse);
-    return textResponse;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  //   const data = await response.json();
+  //   const textResponse = data.choices[0]?.message?.content ?? "";
+ const textResponse = "Are you struggling to get a good night's sleep? Let's explore some tips for better sleep. Create a routine by going to bed and waking up at the same time every day. Make your bedroom conducive to sleep by keeping it cool, dark, and quiet. Limit screen time before bed to reduce exposure to blue light, which can disrupt your sleep. Try relaxation techniques like deep breathing or meditation to calm your mind before bedtime. Avoid heavy meals, caffeine, and alcohol close to bedtime for a better night's rest. Stay active during the day to promote better sleep at night. Remember, good sleep hygiene is essential for overall health and well-being. Implement these tips and start enjoying a more restful night's sleep tonight.";
+  //   if (!textResponse) {
+  //     //handle case of empty resoult
+  //     console.log("response was empty");
+  //     return null;
+  //   }
+  //   console.log("textResponse: " + textResponse);
+    return textRespons;
+  // } catch (error) {
+  //   console.error(error);
+  //   return null;
+  // }
 };
 
 module.exports = {
